5838fdb7bb3ad8f53f666d0fc82d6074cd0df57d
Wayland Harwood <waylandharwood@gmail.com>
2019-04-16 05:59:01 +0900

    change to html

---
+++ js/extend.js
@@ -0,0 +1,11 @@
+$(function() {
+
+var themeArray = ['orange-theme', 'blue-theme'];
+
+var randomValue = themeArray[Math.floor(Math.random() * themeArray.length)];
+
+console.log(randomValue);
+
+$('body').addClass(randomValue);
+
+});
